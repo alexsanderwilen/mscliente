0c11d7ad0198faff01635d376ec691859a7f1201
Alexsander <alexsanderwilen@sancode.com.br>
2022-09-06 10:51:19 -0300

    teste connect eureka server

--- src/main/java/br/com/sancode/mscliente/application/resource/ClienteResource.java
+++ src/main/java/br/com/sancode/mscliente/application/resource/ClienteResource.java
@@ -1,15 +1,31 @@
 package br.com.sancode.mscliente.application.resource;
 
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.InstanceInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClienteResource {
+
+
+    private final ApplicationInfoManager applicationInfoManager;
+
 
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    @GetMapping("/address")
+    public String getAddress(){
+        InstanceInfo appInfo = applicationInfoManager.getInfo();
+        return appInfo.getHostName();
     }
 }
